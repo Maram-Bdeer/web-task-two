cb9aeaffe0a141c13e6c6c03ec6c972c6b69a474
Maram <marambasem18@gmail.com>
2021-03-14 11:19:40 +0200

    create project

---
+++ taskT-service/app.js
@@ -0,0 +1,72 @@
+/*var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/deals');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/deals', dealsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;*/
+const express = require('express')
+const bodyParser=require('body-parser')
+const app = express()
+const port = 3000
+app.use(bodyParser.json());
+const student=['m','a','l']
+
+app.get('/',(req,res)=>{
+  res.status(200).json(student);
+})
+
+app.post('/',(req,res)=>{
+  let requestBody=req.body;
+  console.log('object',req)
+  let studentName=requestBody.name;
+  student.push(requestBody);
+
+  res.status(201).json(studentName)
+})
+
+app.delete()
+
+app.put()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
